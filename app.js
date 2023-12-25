cee5b9f2fa43a4197feb605ee80b06ff2bd349a0
Alekhya.E <alekhya.e@alekhya.e-mac>
2023-12-25 10:12:03 +0530

    Episode -2

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(header);
 
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const header= React.createElement("div", {id:"parent"}, 
                 React.createElement("div", {id:"child"},
                 [React.createElement("h1",{},'I am head1'),
